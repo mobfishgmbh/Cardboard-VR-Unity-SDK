729aa4b833203ba5a83d73bdc2eea35e0e8a9fe5
Rohan Tipare <rohantipare@gmail.com>
2020-04-27 16:25:41 +0530

    Replace libCardboardUtility.a with native source files

---
+++ Assets/Cardboard/Plugins/iOS/libCardboardUtility/NotificationObserver.m
@@ -0,0 +1,32 @@
+#import "NotificationObserver.h"
+#define kQRCodeScannedNotification @"QRCodeScanned"
+
+@implementation NotificationObserver
+
+static QRScannedCallback qrScannedCallback;
++(QRScannedCallback) qrScannedCallback {
+    @synchronized (self) {
+        return qrScannedCallback;
+    }
+}
++(void) setQrScannedCallback: (nullable QRScannedCallback) _callback{
+    @synchronized (self) {
+        qrScannedCallback = _callback;
+    }
+}
+
++(void) RegisterQRCallback:(nullable QRScannedCallback) _callback{
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(QRObserver:) name:kQRCodeScannedNotification object:nil];
+    NotificationObserver.qrScannedCallback = _callback;
+}
++(void) DeRegisterQRCallback{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    NotificationObserver.qrScannedCallback = nil;
+}
++(void) QRObserver: (NSNotification *) name{
+    NSLog(@"QRCodeScanned Received!!");
+    if(NotificationObserver.qrScannedCallback != nil){
+        NotificationObserver.qrScannedCallback();
+    }
+}
+@end
