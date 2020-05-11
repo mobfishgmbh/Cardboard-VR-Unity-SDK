dffe3ca4ed52ae2f3b2170331954c9392b84a85f
Rohan Tipare <rohantipare@gmail.com>
2020-05-11 12:13:36 +0530

    Add ability to recenter camera

---
+++ Assets/RecenterDemo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MobfishCardboard;
+
+public class RecenterDemo : MonoBehaviour
+{
+	public float recenterInterval = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(recenterRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator recenterRoutine(){
+    	WaitForSeconds wait = new WaitForSeconds(recenterInterval);
+
+    	while(true){
+    		yield return wait;
+    		CardboardHeadTracker.RecenterCamera();	
+    	}
+    	
+    }
+}
