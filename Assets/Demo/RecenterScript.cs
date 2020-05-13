751964f76dd4bb1a6b4b661483905f718237386a
cai_mobfish <cai@mobfish.net>
2020-05-13 16:31:48 +0200

    Add a button to demostrate recenter.

---
+++ Assets/Demo/RecenterScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using MobfishCardboard;
+
+namespace MobfishCardboardDemo
+{
+    public class RecenterScript : MonoBehaviour
+    {
+        public Button recenterButton;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (recenterButton != null)
+                recenterButton.onClick.AddListener(RecenterCamera);
+
+        }
+
+        // Update is called once per frame
+        void RecenterCamera()
+        {
+            CardboardHeadTracker.RecenterCamera();
+        }
+    }
+}
