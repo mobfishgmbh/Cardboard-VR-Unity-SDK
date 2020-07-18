5a86a9d091c23353373a8bf1ff4b57c47a832360
cdytoby <cdytoby@live.cn>
2020-07-18 14:57:04 +0200

    Add InputSystem Support for head simulation Add gradle templates for Unity 2019

--- Assets/Cardboard/Scripts/Behaviours/SimulateHeadTransform.cs
+++ Assets/Cardboard/Scripts/Behaviours/SimulateHeadTransform.cs
@@ -2,57 +2,107 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+#if UNITY_2019_1_OR_NEWER && ENABLE_INPUT_SYSTEM
+using UnityEngine.InputSystem;
+#endif
+
 namespace MobfishCardboard
 {
-    public class SimulateHeadTransform: MonoBehaviour
-    {
-        [SerializeField]
-        private Transform targetTransform;
+	public class SimulateHeadTransform: MonoBehaviour
+	{
+		[SerializeField]
+		private Transform targetTransform;
 
-        private void Awake()
-        {
-            if (targetTransform == null)
-                targetTransform = GetComponent<Transform>();
+		private void Awake()
+		{
+			if (targetTransform == null)
+				targetTransform = GetComponent<Transform>();
 
-            if (!Application.isEditor)
-                enabled = false;
-        }
+			if (!Application.isEditor)
+				enabled = false;
+		}
 
-        // Start is called before the first frame update
-        void Start()
-        {
+		// Start is called before the first frame update
+		void Start()
+		{
 
-        }
+		}
 
-        // Update is called once per frame
-        void Update()
-        {
-            if (Input.GetKey(KeyCode.LeftAlt))
-            {
-                Vector3 currentEulerAngle = transform.localEulerAngles;
-                float targetRotX = currentEulerAngle.x - Input.GetAxis("Mouse Y");
-                if (targetRotX < 90 || targetRotX > -90)
-                {
-                    currentEulerAngle.x = targetRotX;
-                }
-                float targetRotY = currentEulerAngle.y + Input.GetAxis("Mouse X");
-                if (targetRotY > 360)
-                    targetRotY -= 360;
-                else if (targetRotY < -360)
-                    targetRotY += 360;
-                currentEulerAngle.y = targetRotY;
+		// Update is called once per frame
+		void Update()
+		{
+			if (GetKeyRotate())
+			{
+				Vector3 currentEulerAngle = transform.localEulerAngles;
+				float targetRotX = currentEulerAngle.x - GetMouseY();
+				if (targetRotX < 90 || targetRotX > -90)
+				{
+					currentEulerAngle.x = targetRotX;
+				}
+				float targetRotY = currentEulerAngle.y + GetMouseX();
+				if (targetRotY > 360)
+					targetRotY -= 360;
+				else if (targetRotY < -360)
+					targetRotY += 360;
+				currentEulerAngle.y = targetRotY;
 
-                transform.localEulerAngles = currentEulerAngle;
-            }
-            else if (Input.GetKey(KeyCode.LeftControl))
-            {
-                Vector3 currentEulerAngle = transform.localEulerAngles;
-                float targetRotZ = currentEulerAngle.z - Input.GetAxis("Mouse X");
+				transform.localEulerAngles = currentEulerAngle;
+			}
+			else if (GetKeyTilt())
+			{
+				Vector3 currentEulerAngle = transform.localEulerAngles;
+				float targetRotZ = currentEulerAngle.z - GetMouseY();
 
-                currentEulerAngle.z = targetRotZ;
-                transform.localEulerAngles = currentEulerAngle;
-            }
+				currentEulerAngle.z = targetRotZ;
+				transform.localEulerAngles = currentEulerAngle;
+			}
 
-        }
-    }
+		}
+
+		#if !UNITY_2019_1_OR_NEWER || ENABLE_LEGACY_INPUT_MANAGER
+
+		private bool GetKeyRotate()
+		{
+			return Input.GetKey(KeyCode.LeftAlt);
+		}
+
+		private bool GetKeyTilt()
+		{
+			return Input.GetKey(KeyCode.LeftControl);
+		}
+
+		private float GetMouseX()
+		{
+			return Input.GetAxis("Mouse X");
+		}
+
+		private float GetMouseY()
+		{
+			return Input.GetAxis("Mouse Y");
+		}
+
+		#else
+
+		private bool GetKeyRotate()
+		{
+			return Keyboard.current.altKey.isPressed;
+		}
+
+		private bool GetKeyTilt()
+		{
+			return Keyboard.current.ctrlKey.isPressed;
+		}
+
+		private float GetMouseX()
+		{
+			return Mouse.current.delta.x.ReadValue();
+		}
+
+		private float GetMouseY()
+		{
+			return Mouse.current.delta.y.ReadValue();
+		}
+
+		#endif
+	}
 }
