aa8acb41cd746f74bb131ff16b7a6ebcae34fb21
cdytoby <cdytoby@live.cn>
2020-07-18 12:25:15 +0200

    Change targetFramerate on Android to 60 too.

---
+++ Assets/Demo/FramerateCount.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FramerateCount: MonoBehaviour
+{
+    public static string fpsString { get; private set; }
+
+    private float deltaTime = 0;
+
+    void Update()
+    {
+        float fps = 1.0f / Time.deltaTime;
+        fpsString = Mathf.Ceil(fps).ToString();
+    }
+}
